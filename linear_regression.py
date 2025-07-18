d20209b8961b4fc7a35c7eb4194ac3cb58405a78
sergioo-herreraa <112037477+sergioo-herreraa@users.noreply.github.com>
2025-07-18 12:05:17 -0600

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -2,7 +2,7 @@ import numpy as np
 
 ### Functions for you to fill in ###
 
-def closed_form(X, Y, lambda_factor):
+
 def closed_form(X, Y, lambda_factor):
     """
     Computes the closed form solution of linear regression with L2 regularization
